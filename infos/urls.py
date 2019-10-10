6149143047aec99b11c78de05a8ab2968b8c1b3b
Bousmaha Wassel <wassel2005@gmail.com>
2019-10-10 17:51:53 +0200

    création d'un modèle option

--- infos/urls.py
+++ infos/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-#	path("edt/<str:id>/<str:password>", views.edt, name="edt"),
-	path("edt", views.edt, name="edt"),
+	path("edt/<str:id>/<str:password>", views.edt, name="edt"),
 ]
