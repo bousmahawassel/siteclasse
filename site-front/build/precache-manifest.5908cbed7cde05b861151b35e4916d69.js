34ab76e6e897632984474255930f3c626a398115
wasselbousmaha <wassel2005@gmail.com>
2019-11-12 18:40:08 +0100

    Création du build de prod

---
+++ site-front/build/precache-manifest.5908cbed7cde05b861151b35e4916d69.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfa67d5158349f43949591105f0b0871",
+    "url": "/index.html"
+  },
+  {
+    "revision": "778f834164bfd9375647",
+    "url": "/static/css/2.2a598887.chunk.css"
+  },
+  {
+    "revision": "a18275214378e50dd654",
+    "url": "/static/css/main.9c323d60.chunk.css"
+  },
+  {
+    "revision": "778f834164bfd9375647",
+    "url": "/static/js/2.49a8d0fe.chunk.js"
+  },
+  {
+    "revision": "a18275214378e50dd654",
+    "url": "/static/js/main.8102a4c3.chunk.js"
+  },
+  {
+    "revision": "8da6922badfccd4e5f6c",
+    "url": "/static/js/runtime-main.05c7dc91.js"
+  },
+  {
+    "revision": "d1890b860c824f447d44d7865b572699",
+    "url": "/static/media/Logo_classe.d1890b86.png"
+  },
+  {
+    "revision": "f5331cb6372b6c0d8baf2dd7e200498c",
+    "url": "/static/media/OpenSans-Bold.f5331cb6.ttf"
+  },
+  {
+    "revision": "d7d5d4588a9f50c99264bc12e4892a7c",
+    "url": "/static/media/OpenSans-Regular.d7d5d458.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
